ed80acccd1ffcc5e55e4e0180612b15c25ea8b1e
Kamrujjaman <mrkamrul75251@gmail.com>
2025-03-28 17:29:21 +0800

    compelet User Input or Login section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 module.exports = {
-  content: ['./index.html'],
   theme: {
     extend: {
-      colors: {
-        clifford: '#da373d',
-        primary: '#8bc34a',
-        secondary: '#111111',
-        body: '#f8f6f3',
-        accent: '#6a9739',
-        body_neutral: '#153333',
+      backgroundImage: {
+        'local-image': "url('./images/image.jpg')",
       },
     },
   },
-  plugins: [require('daisyui')],
 };
